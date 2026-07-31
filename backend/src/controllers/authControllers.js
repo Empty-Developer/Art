b0bd855fadbe36f7b1aa9c98c4c9fccd0a5221f3
Empty <emptypageworking@gmail.com>
2026-07-31 03:23:19 +0300

    Fix(try): added check

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -2,42 +2,46 @@ import { prisma } from "../config/db.js";
 import bcrypt from "bcryptjs";
 
 const registration = async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  // check if user already exists
-  const userExist = await prisma.user.findUnique({
-    where: { email: email },
-  });
+    // check if user already exists
+    const userExist = await prisma.user.findUnique({
+      where: { email: email },
+    });
 
-  if (userExist) {
-    return res
-      .status(400)
-      .json({ error: "user already exists witch this email" });
-  }
+    if (userExist) {
+      return res
+        .status(400)
+        .json({ error: "user already exists witch this email" });
+    }
 
-  // hash password
-  const salt = await bcrypt.genSalt(12);
-  const hashedPassword = await bcrypt.hash(password, salt);
+    // hash password
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-  // create user
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-    },
-  });
-
-  res.status(201).json({
-    status: "success",
-    data: {
-      user: {
-        id: user.id,
-        name: name,
-        email: email,
+    // create user
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
       },
-    },
-  });
+    });
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: {
+          id: user.id,
+          name: name,
+          email: email,
+        },
+      },
+    });
+  } catch (error) {
+    res.status(401);
+  }
 };
 
 export { registration };
